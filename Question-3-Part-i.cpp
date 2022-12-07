34e792121d52e15dc3ee36dcdae13ba950d0c718
Bernard <kimaryobernard@gmail.com>
2022-12-07 12:23:59 +0300

    Question 3 part i

---
+++ Question-3-Part-i.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//check if number is even
+bool checkEvent(int number)
+{
+    if(number%2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+int number;
+cin>>number;
+
+if(checkEvent(number)==true)
+{    
+cout<<"The number is Even"<<endl;
+}else{
+cout<<"The number is Odd"<<endl;
+}
+
+return 0;
+}
