3c65ff4f61dc4c61b9c7ff783ae1c589f255641e
Bernard <kimaryobernard@gmail.com>
2022-12-07 12:33:54 +0300

    Question 3

---
+++ Question-3-Part-ii.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+//check if number is even
+int max(int number1, int number2, int number3)
+{
+    int maxnumber;
+
+    if(number1> number2 && number1> number3)
+    {
+        maxnumber = number1;
+    }
+    else if(number2>number1 && number2>number3)
+    {
+        maxnumber=number2;
+    }
+    else
+    {
+        maxnumber=number3;
+    }
+    return maxnumber;
+}
+
+int main()
+{
+int number1;
+int number2;
+int number3;
+
+cout<<"Please enter three numbers"<<endl;
+
+cin>>number1;
+cin>>number2;
+cin>>number3;
+
+cout<<"The greatest number is "<<max(number1,number2,number3)<<endl;
+
+system("pause");
+return 0;
+}
